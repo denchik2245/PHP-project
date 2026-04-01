f63b78ce95fc4b9cff176aab803b58d3c64d1fd5
Tomaso1420 <147404719+Tomaso1420@users.noreply.github.com>
2026-04-01 13:55:57 +0500

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Article;
 use App\Models\Movie;
 use Illuminate\Contracts\View\View;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends Controller
 {
@@ -23,5 +24,26 @@ class HomeController extends Controller
             ->get();
 
         return view('home', compact('movies', 'articles'));
+    }
+
+    public function error403(): Response
+    {
+        return response()
+            ->view('errors.403', [], 403)
+            ->header('X-Demo-Error', 'Forbidden');
+    }
+
+    public function error404(): Response
+    {
+        return response()
+            ->view('errors.404', [], 404)
+            ->header('X-Demo-Error', 'Not-Found');
+    }
+
+    public function error500(): Response
+    {
+        return response()
+            ->view('errors.500', [], 500)
+            ->header('X-Demo-Error', 'Server-Error');
     }
 }

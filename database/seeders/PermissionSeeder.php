2e20b13ccddbd513e9cff8a5cabf3b6190b85417
Tomaso1420 <147404719+Tomaso1420@users.noreply.github.com>
2026-04-01 13:49:10 +0500

    Update PermissionSeeder.php

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -2,16 +2,33 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Permission;
 use Illuminate\Database\Seeder;
 
 class PermissionSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $permissions = [
+            ['name' => 'movie.view', 'title' => 'Просмотр фильмов'],
+            ['name' => 'movie.create', 'title' => 'Создание фильмов'],
+            ['name' => 'movie.update', 'title' => 'Редактирование фильмов'],
+            ['name' => 'movie.delete', 'title' => 'Удаление фильмов'],
+
+            ['name' => 'article.view', 'title' => 'Просмотр статей'],
+            ['name' => 'article.create', 'title' => 'Создание статей'],
+            ['name' => 'article.update.own', 'title' => 'Редактирование своих статей'],
+            ['name' => 'article.publish', 'title' => 'Публикация статей'],
+
+            ['name' => 'user.manage', 'title' => 'Управление пользователями'],
+            ['name' => 'role.manage', 'title' => 'Управление ролями'],
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::updateOrCreate(
+                ['name' => $permission['name']],
+                ['title' => $permission['title']]
+            );
+        }
     }
 }

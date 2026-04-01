162e712a532b79493516b28599f6724ae848b1d4
Tomaso1420 <147404719+Tomaso1420@users.noreply.github.com>
2026-04-01 13:50:04 +0500

    Update RoleSeeder1.php

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Permission;
 use App\Models\Role;
 use Illuminate\Database\Seeder;
 
@@ -15,11 +16,45 @@ class RoleSeeder extends Seeder
             ['name' => 'author', 'title' => 'Автор'],
         ];
 
-        foreach ($roles as $role) {
+        foreach ($roles as $roleData) {
             Role::updateOrCreate(
-                ['name' => $role['name']],
-                ['title' => $role['title']]
+                ['name' => $roleData['name']],
+                ['title' => $roleData['title']]
             );
         }
+
+        $admin = Role::where('name', 'admin')->first();
+        $editor = Role::where('name', 'editor')->first();
+        $author = Role::where('name', 'author')->first();
+
+        $adminPermissions = Permission::whereIn('name', [
+            'movie.view',
+            'movie.create',
+            'movie.update',
+            'movie.delete',
+            'article.view',
+            'article.create',
+            'article.update.own',
+            'article.publish',
+            'user.manage',
+            'role.manage',
+        ])->pluck('id');
+
+        $editorPermissions = Permission::whereIn('name', [
+            'movie.view',
+            'article.view',
+            'article.publish',
+        ])->pluck('id');
+
+        $authorPermissions = Permission::whereIn('name', [
+            'movie.view',
+            'article.view',
+            'article.create',
+            'article.update.own',
+        ])->pluck('id');
+
+        $admin->permissions()->sync($adminPermissions);
+        $editor->permissions()->sync($editorPermissions);
+        $author->permissions()->sync($authorPermissions);
     }
 }
